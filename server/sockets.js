367512ab3eccdcec9ad5e833118740bf5a702d5c
g3v0 <gevogk@gmail.com>
2018-04-29 20:50:36 -0700

    more dank

--- server/sockets.js
+++ server/sockets.js
@@ -8,24 +8,31 @@ module.exports = (server,db) => {
     // const projects = [];
     let counter = 0
 
-    const projects = [
-        new Project(counter++,'berlin wall','',[ new ToDo(0,'divide germany into 4 parts','',false)]),
-        new Project(counter++, 'robot waifus','', [new ToDo(0,'robot titties','',false)]),
-        new Project(counter++, 'laser beam sharks','', [new ToDo(0,'find sharks','',false)]),
-    ];
+    // const projects = [
+    //     new Project(counter++,'berlin wall','',[ new ToDo(0,'divide germany into 4 parts','',false)]),
+    //     new Project(counter++, 'robot waifus','', [new ToDo(0,'robot titties','',false)]),
+    //     new Project(counter++, 'laser beam sharks','', [new ToDo(0,'find sharks','',false)]),
+    // ];
 
     // when the page is loaded in the browser the connection event is fired
     io.on('connection', socket => {
 
-        // on making a connection send all the projects
-        socket.emit('send-projects', projects);
+
+        db.getAllProjects().then(projects =>
+            // on making a connection refresh all the projects
+            socket.emit('refresh-projects', projects))
 
         socket.on('add-project', projectname => {
             //add the project to the list
             console.log("add project")
             const project = new Project(counter++, projectname, '', [])
-            projects.push(project)
-            db.addProject(project).then(() => io.emit('send-projects', projects))
+            // projects.push(project)
+            db.addProject(project).then(project => {
+                db.getAllProjects().then(projects => {
+                    socket.emit('refresh-projects', projects)
+                    console.log(projects)
+                })
+            })
         });
         socket.on('remove-completed-todos', data => {
             //remove the completed todos of a project
